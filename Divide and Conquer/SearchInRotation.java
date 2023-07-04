523f44bccde5765ee70fda6263dc6f020169f41e
Ashish Bhati <ashbhati26@gmail.com>
2023-07-05 00:54:16 +0530

    02

--- Divide and Conquer/SearchInRotation.java
+++ Divide and Conquer/SearchInRotation.java
@@ -1,6 +1,38 @@
 public class SearchInRotation {
-    public static void main(String[] args) {
-        
+    public static int search(int arr[], int tar, int si, int ei){
+        if(si > ei){
+            return -1;
+        }
+        int mid = si + (ei - si)/2;
+        // case Found
+        if(arr[mid] == tar){
+            return mid;
+        }
+        // mid on L1
+        if(arr[si] <= arr[mid]){
+            // case a : left
+            if(arr[si] <= tar && tar <= arr[mid]){
+                return search(arr, tar, si, mid-1);
+            } else {
+                // case b: right
+                return search(arr, tar, mid+1, ei);
+            }
+        }
+        // mid on L2
+        else{
+            //  Case C: right
+            if(arr[mid] <= tar && tar <= arr[ei]){
+                return search(arr, tar, mid+1, ei);
+            } else{
+                // case D: left
+                return search(arr, tar, si, mid-1);
+            }
+        }
     }
-    
+    public static void main(String[] args) {
+        int arr[] = {4, 5, 6, 0, 1, 2};
+        int target = 6; // output
+        int tarIdx = search(arr, target, 0, arr.length-1);
+        System.out.println(tarIdx);
+    }
 }
