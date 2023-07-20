49a6071f5b987c2405cc820b440af3777216e8c0
Ashish Bhati <ashbhati26@gmail.com>
2023-07-20 19:11:30 +0530

    04

--- Linked Lists/LinkedList.java
+++ Linked Lists/LinkedList.java
@@ -216,16 +216,104 @@ public class LinkedList {
         return true;
     }
 
-    public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.addFirst(2);
-        ll.addFirst(1);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.add(2, 3);
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; //mid node
+    }
 
-        ll.print(); // 1->2->3->4->5
-        System.out.println(ll.checkPalindrome());
+    private Node merge(Node head1, Node head2){
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while(head1 != null && head2 != null){
+            if(head1.data <= head2.data){
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+        while(head1 != null){
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+        return mergedLL.next;
+    }
+
+    public Node mergeSort(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        // find mid
+        Node mid = getMid(head);
+        // left and right merge shot
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+        // merge
+        return merge(newLeft, newRight);
+    }
+
+    public void ZigZag(){
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+        // reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        // alt merge - zig zag merge
+        while(left != null && right != null){
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String[] args) {
+        // LinkedList ll = new LinkedList();
+        // ll.addFirst(2);
+        // ll.addFirst(1);
+        // ll.addLast(4);
+        // ll.addLast(5);
+        // ll.add(2, 3);
+
+        // ll.print(); // 1->2->3->4->5
+        // System.out.println(ll.checkPalindrome());
 
         // ll.deleteNthfromEnd(3);
         // ll.print();
@@ -246,6 +334,29 @@ public class LinkedList {
         // ll.print();
         // System.out.println(ll.size);
 
+        // merge sort
+        // LinkedList ll = new LinkedList();
+        // ll.addFirst(1);
+        // ll.addFirst(2);
+        // ll.addFirst(3);
+        // ll.addFirst(4);
+        // ll.addFirst(5);
+        // // 5->4->3->2->1
+        // ll.print();
+        // ll.head = ll.mergeSort(ll.head);
+        // ll.print();
 
+        // ZigZag
+        LinkedList ll = new LinkedList();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+        // 1->2->3->4->5
+        ll.print();
+        ll.ZigZag();
+        ll.print();
     }   
-}
+} 
