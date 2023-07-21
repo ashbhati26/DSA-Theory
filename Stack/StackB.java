092147729f26479e543f780a06b695490b11e3d5
Ashish Bhati <ashbhati26@gmail.com>
2023-07-21 23:30:04 +0530

    05

---
+++ Stack/StackB.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class StackB {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size() = 0;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
