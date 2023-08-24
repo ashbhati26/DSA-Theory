ddbda4787ae122b26a6f2312a9088eddd8125e6a
Ashish Bhati <ashbhati26@gmail.com>
2023-08-24 23:23:40 +0530

    07

--- Binary Search Trees/Merge2BSTs.java
+++ Binary Search Trees/Merge2BSTs.java
@@ -1,6 +1,35 @@
+import java.util.ArrayList;
+
 public class Merge2BSTs {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
+    public static void getInorder(Node root, ArrayList<Integer> arr){
+        if(root == null){
+            return;
+        }
+        getInorder(root.left, arr);
+        arr.add(root.data);
+    }
+
+    public static Node mergeBSTs(Node root1, Node root2){
+        getInorder(root1)
+    }
     public static void main(String[] args) {
-        
+        Node root1 = new Node(2);
+        root1.left = new Node(1);
+        root1.right = new Node(4);
+
+        Node root2 = new Node(9);
+        root2.left = new Node(3);
+        root2.right = new Node(12);
     }
     
 }
